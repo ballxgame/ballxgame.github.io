9cca20332dfb5bc0f3417f1eeb5b9b72ba0b8bba
ballxgame <36086755+ballxgame@users.noreply.github.com>
2018-03-05 21:17:01 -0600

    Update main_out.js

--- assets/js/main_out.js
+++ assets/js/main_out.js
@@ -1,6 +1,6 @@
 (function(wHandle, wjQuery) {
     /*global navigator, Image, $*/
-    var CONNECTION_URL = "127.0.0.1:443"; // Default Connection IP
+    var CONNECTION_URL = "agarserv-test-feedy123.c9users.io/"; // Default Connection IP
     var SKIN_URL = "./skins/"; // Skins Directory
     wHandle.setServer = function(arg) {
         if (arg != gameMode) {
